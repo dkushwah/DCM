01738aea905863a462811af02d2c796c7797a919
Sarita <sarita.170317@gmail.com>
2017-09-23 15:19:08 +0530

    DCM: WebAPI integration

---
+++ DigitalCultivationMart.API/Controllers/DCMController.cs
@@ -0,0 +1,63 @@
+﻿using DigitalCultivationMart.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class DCMController : ApiController
+    {
+        private readonly DigitalCultivationMart.BizLayer.Contracts.IDCMManager dcmManager;
+        private readonly DigitalCultivationMart.BizLayer.Contracts.IAccountManager accountManager;
+        public DCMController()
+        {
+            dcmManager = new DigitalCultivationMart.BizLayer.Impl.DCMManager();
+            accountManager = new DigitalCultivationMart.BizLayer.Impl.AccountManager();
+        }
+
+        [Route("UserLogin")]
+        public async Task<IHttpActionResult> UserLogin(string username, string password)
+        {
+            var retval = accountManager.IsAccountExists(username, password);
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("CultivationInfoDetails")]
+        public async Task<CultivationDetailDTO> AddCultivationInfoDetails(CultivationDetailDTO cultivationDetails)
+        {
+            var retval = dcmManager.AddCultivationDetails(cultivationDetails);
+            return retval;
+        }
+
+
+        [HttpPut]
+        [Route("CultivationInfoDetails")]
+        public async Task<IHttpActionResult> UpdateCultivationInfoDetails(CultivationDetailDTO cultivationDetails)
+        {
+            var retval = dcmManager.UpdateCultivationDetails(cultivationDetails);
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("CultivationInfoDetails")]
+        public IEnumerable<CultivationDetailDTO> GetAllCultivationDetails()
+        {
+            return dcmManager.GetAllCultivationDetails();
+        }
+
+        [HttpGet]
+        [Route("GetStates")]
+        public IEnumerable<StateDTO> GetStates()
+        {
+            return dcmManager.GetStates();
+
+        }
+
+
+
+    }
+}
---
+++ DigitalCultivationMart.API/Models/CultivationDetails.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class CultivationDetails
+    {
+        public int SerialNumber { get; set; }
+
+        public int StateId { get; set; }
+
+        public string Year { get; set; }
+
+        public int CultivationArea { get; set; }
+
+        public string CropName { get; set; }
+
+        public Decimal QuantityOfCultivation { get; set; }
+
+        public int Demand { get; set; }
+
+        public int Supply { get; set; }
+    }
+}
