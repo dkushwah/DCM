073c7e595427ae18b78b4e402adfc8a798ae6590
Sarita <sarita.170317@gmail.com>
2017-09-23 10:37:09 +0530

    DCM: Project setup

---
+++ DigitalCultivationMart/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DigitalCultivationMart.Startup))]
+namespace DigitalCultivationMart
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
